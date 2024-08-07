6c7949f9dab7ca081da1d3dab4b620c34178a652
Kihyun Kim <166822848+0531Kim@users.noreply.github.com>
2024-08-08 09:33:20 +1200

    [Bronze II] Title: 농구 경기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/1159. 농구 경기/농구 경기.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int num, flag, cnt[200];
+string name;
+int main(){
+	cin >> num;
+	for(int i = 0; i < num; i++){
+		cin >> name;
+		cnt[name[0] - 'a']++;
+	}
+	for(int i = 0; i < 30; i++){
+		if(cnt[i] >= 5){
+			cout << char(i + 'a');
+			flag = 1;
+		}
+	}
+	if(flag == 0){
+		cout << "PREDAJA";
+	}
+	return 0;
+}
