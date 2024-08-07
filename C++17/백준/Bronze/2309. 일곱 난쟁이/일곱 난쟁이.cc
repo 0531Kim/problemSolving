cf5906e122c57eaec1bec9e2c125dda369117f02
Kihyun Kim <166822848+0531Kim@users.noreply.github.com>
2024-08-08 09:40:55 +1200

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[50], sum, a, b;
+vector<int> v;
+
+int main()
+{
+    for (int i = 0; i < 9; i++)
+    {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if ((arr[i] + arr[j]) == (sum - 100))
+            {
+                a = arr[i];
+                b = arr[j];
+                break;
+            }
+        }
+    }
+
+    for (int i = 0; i < 9; i++)
+    {
+        if ((arr[i] != a) && (arr[i] != b))
+            v.push_back(arr[i]);
+    }
+
+    sort(v.begin(), v.end());
+
+    for (int i : v)
+        cout << i << '\n';
+
+    return 0;
+}
